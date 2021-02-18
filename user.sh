674ab7da422f90fc8924119d679c576d9495fae6
jkread <jr@johnkread.com>
2021-02-17 22:31:52 -0600

    Create user.sh

---
+++ user.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Change the UID if needed
+if [ ! "$(id -u steam)" -eq "$UID" ]; then
+	echo "Changing steam uid to $UID."
+	usermod -o -u "$UID" steam ;
+fi
+# Change gid if needed
+if [ ! "$(id -g steam)" -eq "$GID" ]; then
+	echo "Changing steam gid to $GID."
+	groupmod -o -g "$GID" steam ;
+fi
+
+# Add Ark Server Tools to Path
+export PATH=$PATH:/home/steam/ark-server-tools/:/home/steam/ark-server-tools/tools/
+
+
+# Put steam owner of directories (if the uid changed, then it's needed)
+chown -R steam:steam /ark /home/steam
+
+# avoid error message when su -p (we need to read the /root/.bash_rc )
+chmod -R 777 /root/
+
+# Launch run.sh with user steam (-p allow to keep env variables)
+sudo --preserve-env --user=steam /home/steam/run.sh
